6ce92eed97abcfb274e2f085dd9d796ec122f581
g1sel3e <giselesilva200809@gmail.com>
2026-06-16 12:48:18 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,12 +1,13 @@
-conexao.php
 <?php
+$host = "mysql-wasabido.alwaysdata.net"; 
+$dbname = "wasabido_a";          
+$user = "wasabido";         
+$password = "2008wasabido@"; // Coloque aqui a senha que você usa para entrar na Alwaysdata
 
-	$host = "localhost";
-	$banco = "wasabido_a";
-	$user = "root";
-	$pass = "";
-
-	$conexao = new PDO("mysql:host=$host; dbname=$banco",$user,$pass);
-
-	
+try {
+    $conexao = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+}
 ?>
